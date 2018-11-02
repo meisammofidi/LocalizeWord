234d2ff7406a9332a21142c34db07672affc7cf2
meisammofii <meisam.mofidi@yahoo.com>
2018-11-02 15:56:36 -0400

    Initial Commit

---
+++ LocalizeWord.API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using LocalizeWord.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LocalizeWord.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Value> Values { get; set; }
+
+    }
+}
